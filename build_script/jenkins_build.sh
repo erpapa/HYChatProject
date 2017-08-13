30d425bdda4e237c87cbeeaadbfdd12834faaa7e
erpapa <hyplcf@163.com>
2017-08-13 14:53:22 +0800

    test

---
+++ build_script/jenkins_build.sh
@@ -0,0 +1,156 @@
+#!/bin/sh
+
+RestorePlistFile()
+{
+	if [[ ! -z "$origin_bundle_version" ]] && [[ ! -z "$info_plist_path" ]]; then
+		/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${origin_bundle_version}" "${info_plist_path}"
+	fi
+
+	if [[ ! -z "$info_string_path" ]] && [[  -e "$info_string_path".bak ]]; then
+		mv "$info_string_path".bak $info_string_path
+	fi
+}
+
+#统一fail处理函数
+Failed()
+{
+    echo "Failed: $*"
+    RestorePlistFile
+    exit -1
+}
+
+Build()
+{
+	mkdir -p ${output_dir}
+	
+	version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${info_plist_path}")
+	origin_bundle_version=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${info_plist_path}")
+	/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${version}.${build_number}" "${info_plist_path}"
+
+	app_name="ColorV-${server_env}-${build_type}-${version}.${build_number}"
+	app_file_path="${output_dir}/${app_name}.app"
+	ipa_file_path="${output_dir}/${app_name}.ipa"
+	sym_file_path="${output_dir}/${app_name}.app.dSYM"
+	final_output_dir="${build_root_path}/${app_name}"
+
+	#进入工程目录
+	cd "${src_path}"
+
+	#开始编译操作
+
+	echo "Build and Archive Run Target..."
+
+
+	"${xcodePath}/xcodebuild" -target "${target}" -configuration "${configuration}" -sdk "${sdk}" clean || Failed "Clean Run Target"
+	"${xcodePath}/xcodebuild" -target "${target}" -configuration "${configuration}" -sdk "${sdk}" CONFIGURATION_BUILD_DIR="${build_output_dir}" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}" PROVISIONING_PROFILE_SPECIFIER="${PROVISIONING_PROFILE}" || Failed "Build Run Target"
+
+	mv "${build_app_file_path}" "${app_file_path}"
+	mv "${build_sym_file_path}" "${sym_file_path}"
+
+	echo "Build and Archive Run Target end"
+
+	if [ ! -d "${output_dir}" ];then
+	    Failed "No build directory"
+	fi
+
+	#开始打包操作
+
+	"${xcodePath}/xcrun" -sdk "$sdk" PackageApplication "${app_file_path}" -o "${ipa_file_path}" || Failed "Package ipa"
+
+	if [[ ! -z "$final_output_dir" ]]  && [[ ! -z "$root_path" ]]; then
+		rm -rf ${final_output_dir}
+	fi
+
+	if [[ ! -z "$build_output_dir" ]]  && [[ ! -z "$root_path" ]]; then
+		rm -rf ${build_output_dir}
+	fi
+
+	mv "${output_dir}" "${final_output_dir}" || Failed "Rename"
+
+	RestorePlistFile
+}
+
+jenkins_build_type=$1 # Test Online Release
+
+if [ -z $jenkins_build_type ]; then
+	jenkins_build_type="Release"
+fi
+
+build_number=$2
+
+if [ -z $build_number ]; then
+	build_number=0
+fi
+
+root_path="`dirname $0`/.."
+root_path_prefix=${root_path:0:1}
+if [[ "$root_path_prefix" != "/" ]]; then
+	root_path="`pwd`/${root_path}"
+fi
+# root_path="`pwd`/.."
+xcodePath=/usr/bin
+
+src_path="${root_path}"
+cert_path="${root_path}/cert"
+comment_file_path="${root_path}/comment.txt"
+info_plist_path="${src_path}/HYChatProject/Info.plist"
+info_string_path="${src_path}/HYChatProject/zh-Hans.lproj/InfoPlist.strings"
+
+build_root_path="${root_path}/output/Build_${build_number}"
+build_output_dir="${build_root_path}/build"
+output_dir="${build_root_path}/tmp"
+build_app_file_path="${build_output_dir}/HYChatProject.app"
+build_sym_file_path="${build_output_dir}/HYChatProject.app.dSYM"
+
+sdk="iphoneos"
+target="colorv"
+
+adhoc_pp_uuid=`/usr/libexec/PlistBuddy -c "Print UUID" /dev/stdin <<< $(/usr/bin/security cms -D -i ${cert_path}/adhoc.mobileprovision)`
+dis_pp_uuid=`/usr/libexec/PlistBuddy -c "Print UUID" /dev/stdin <<< $(/usr/bin/security cms -D -i ${cert_path}/dis.mobileprovision)`
+dev_pp_uuid=`/usr/libexec/PlistBuddy -c "Print UUID" /dev/stdin <<< $(/usr/bin/security cms -D -i ${cert_path}/dev.mobileprovision)`
+
+#删除之前的产出
+
+if [[ ! -z "$build_root_path" ]]  && [[ ! -z "$root_path" ]]; then
+	rm -rf ${build_root_path}
+fi
+
+mkdir -p ${build_root_path}
+cp "${comment_file_path}" "${build_root_path}/comment.txt"
+
+if [[ "$jenkins_build_type" = "Test" ]]; then
+	if [[ ! -z "$info_string_path" ]]; then
+		sed -i '.bak' "s/洽聊/洽聊测试/g" ${info_string_path}
+	fi
+	server_env="Test"
+	build_type="qa"
+	configuration="TestRelease"
+	CODE_SIGN_IDENTITY="iPhone Developer: hyplcf@163.com (6QBWM96MK3)"
+	PROVISIONING_PROFILE="${dev_pp_uuid}"
+
+	Build || Failed "Test"
+fi 
+
+if [[ "$jenkins_build_type" = "Online" ]]; then
+
+	server_env="Online"
+	build_type="qa"
+	configuration="OnlineRelease"
+	CODE_SIGN_IDENTITY="iPhone Developer: hyplcf@163.com (6QBWM96MK3)"
+	PROVISIONING_PROFILE="${adhoc_pp_uuid}"
+
+	Build || Failed "Online"
+fi
+
+if [[ "$jenkins_build_type" = "Release" ]]; then
+
+	server_env="Online"
+	build_type="release"
+	configuration="OnlineRelease"
+	CODE_SIGN_IDENTITY="iPhone Developer: hyplcf@163.com (6QBWM96MK3)"
+	PROVISIONING_PROFILE="${dis_pp_uuid}"
+
+	Build || Failed "Release"
+fi
+
+exit 0
---
+++ build_script/jenkins_package.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+WORKSPACE=$1
+JOB_URL=$2
+BUILD_NUMBER=$3
+build_type=$4
+
+if [ -z $WORKSPACE ]; then
+	exit -1;
+fi
+
+if [ -z $JOB_URL ]; then
+	exit -1;
+fi
+
+if [ -z $BUILD_NUMBER ]; then
+	exit -1;
+fi
+
+if [ -z $build_type ]; then
+	exit -1;
+fi
+
+check_result_path="${WORKSPACE}/check_result.plist"
+
+security unlock-keychain -p h /Users/erpapa/Library/Keychains/login.keychain || exit -1
+
+if [[ -e "$check_result_path" ]]; then
+	rm -f ${check_result_path}
+fi
+
+#${WORKSPACE}/../../SourceChecker ${build_type} ${WORKSPACE} iOS ${check_result_path} || exit -1
+
+sh ${WORKSPACE}/build_script/jenkins_build.sh ${build_type} ${BUILD_NUMBER} || exit -1
+
+#${WORKSPACE}/../../EmailWriter ${WORKSPACE}/output/Build_${BUILD_NUMBER} ${JOB_URL}ws/output/Build_${BUILD_NUMBER} iOS ${check_result_path}
